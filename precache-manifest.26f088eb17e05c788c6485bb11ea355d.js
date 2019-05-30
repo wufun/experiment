a575772cc2f6a57c623bb67d63ff3adeb7a80cd6
Wu Di <wudiac@gmail.com>
2019-05-30 10:12:19 -0700

    Updates

---
+++ precache-manifest.26f088eb17e05c788c6485bb11ea355d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e6b0fe548e7d8da94490d383e3725a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86f115fb69c76baf6e9e",
+    "url": "/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "8d4dbb34bbe51a35bdfa",
+    "url": "/static/css/main.c964e442.chunk.css"
+  },
+  {
+    "revision": "86f115fb69c76baf6e9e",
+    "url": "/static/js/2.166e6493.chunk.js"
+  },
+  {
+    "revision": "8d4dbb34bbe51a35bdfa",
+    "url": "/static/js/main.59f670a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
